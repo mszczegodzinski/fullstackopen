3a12310803dd8aafb247aaaceb312aa9cae74db6
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-07-01 01:15:23 +0200

    feat: add search field and hardcoded people data
    issue: 2.9

--- part2_react/phonebook/src/App.js
+++ part2_react/phonebook/src/App.js
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '12345' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchedPersons, setSearchedPersons] = useState('');
+  const [filteredPersons, setFilteredPersons] = useState([]);
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -25,11 +32,23 @@ const App = () => {
     setNewNumber(currentNumber);
   };
 
+  const onSearchInputChange = (e) => {
+    const currentSearch = e.target.value;
+    setSearchedPersons(currentSearch);
+
+    const filteredPersonsRes = persons.filter((person) => person.name.includes(currentSearch));
+    setFilteredPersons(filteredPersonsRes);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
+          filter shown with <input value={searchedPersons} onChange={onSearchInputChange} />
+        </div>
+        <div>
+          <h2>Add a new</h2>
           <div>
             name: <input value={newName} onChange={onNameInputChange} />
           </div>
@@ -43,9 +62,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person, i) => (
-          <div key={`${i}-person.name}`}>{`${person.name}: ${person.number}`}</div>
-        ))}
+        {searchedPersons.length
+          ? filteredPersons.map((person, i) => (
+              <div key={`${i}-person.name}`}>{`${person.name}: ${person.number}`}</div>
+            ))
+          : persons.map((person, i) => <div key={`${i}-person.name}`}>{`${person.name}: ${person.number}`}</div>)}
       </div>
     </div>
   );
