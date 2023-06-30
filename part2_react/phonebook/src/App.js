e9f2cb15fe096f41fc97a35b513173219ff6b05f
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-07-01 00:42:02 +0200

    feat: add new name to the phonebook
    issue: 2.6

---
+++ part2_react/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    setPersons([...persons, { name: newName }]);
+  };
+
+  const onInputChange = (e) => {
+    const currentName = e.target.value;
+    setNewName(currentName);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={onInputChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, i) => (
+          <div key={`${i}-person.name}`}>{person.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
