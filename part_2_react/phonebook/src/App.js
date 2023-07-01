09151582b1157ec94c99f93d962feb014b685f53
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-07-01 17:11:52 +0200

    feat: fetch initial data from json-server
    issue: 2.11

--- part_2_react/phonebook/src/App.js
+++ part_2_react/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Header } from './components/Header';
 import { Filter } from './components/Filter';
 import { PersonForm } from './components/PersonForm';
 import { Persons } from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
@@ -25,6 +21,7 @@ const App = () => {
       return alert(`${person.name} is already in the phonebook`);
     }
     setPersons([...persons, { name: newName, number: newNumber }]);
+    setNewName('');
   };
 
   const onSearchInputChange = (e) => {
@@ -34,6 +31,15 @@ const App = () => {
     const filteredPersonsRes = persons.filter((person) => person.name.includes(currentSearch));
     setFilteredPersons(filteredPersonsRes);
   };
+
+  useEffect(() => {
+    // prettier-ignore
+    axios
+      .get('http://localhost:3001/persons')
+      .then((res) => {
+        setPersons(res.data);
+      });
+  }, []);
 
   return (
     <div>
