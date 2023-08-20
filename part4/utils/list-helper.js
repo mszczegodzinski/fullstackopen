707520b9ae0db05e114c73017b902364ca9a78bf
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-08-20 19:36:16 +0200

    4.4 feat: add test to total likes for blog list

--- part4/utils/list-helper.js
+++ part4/utils/list-helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const sum = (acc, current) => {
+    return acc + current.likes;
+  };
+  return blogs.reduce(sum, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
